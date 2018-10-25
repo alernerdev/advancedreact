1c75c98f1f1a08dbe3d6a39a829004d6dd5c81d8
alernerdev <alernerdev@gmail.com>
2018-10-25 14:21:25 -0400

    setup yoga server and demonstrated playground interaction with in-memory demo objects

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+// this is the glue between the API and the actual source of data --
+// a rest point or a DB or Prisma
+const Query = {
+    dogs(parent, args, ctx, info) {
+        global.dogs = global.dogs || [];
+        return global.dogs;
+    }
+};
 
 module.exports = Query;
